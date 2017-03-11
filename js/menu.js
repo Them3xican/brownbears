7b5734ddc1aa45e8e79d3754e2727953160d2374
George Houghton <2hougg84@solent.ac.uk>
2017-03-11 14:53:28 +0000

    sortdd

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,4 @@
 $('.menu').click(function () {
-    
-    $('.menu').toggleClass('on', function () {
-        
-    });
-    $('nav').slideToggle(900, function () {
-        
-    });
+    $('.menu').toggleClass('on', function () {});
+    $('nav').slideToggle(900, function () {});
 });
