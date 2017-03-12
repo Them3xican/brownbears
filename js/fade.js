f087bc761decc589406bde44cbdc756864a456f0
George Houghton <2hougg84@solent.ac.uk>
2017-03-12 12:17:50 +0000

    yay

--- js/fade.js
+++ js/fade.js
@@ -1,23 +1,23 @@
-
 var element = document.getElementById("logo-holder");
-var duration = 10000;
-var hidtime = 1000; 
-var showtime = 30000; 
+var duration = 5000;
+var hidtime = 10000;
+var showtime = 30000;
 
 function SetOpa(Opa) {
-  element.style.opacity = Opa;
-  element.style.filter = 'alpha(opacity=' + (Opa * 100) + ');';
+    element.style.opacity = Opa;
+    element.style.filter = 'alpha(opacity=' + (Opa * 100) + ');';
 }
 
 function fadeOut() {
-  for (i = 0; i <= 1; i += 0.01) {
-    setTimeout("SetOpa(" + (1 - i) +")", i * duration);
-  }
-   setTimeout("FadeIn()", (duration + hidtime));
+    for (i = 0; i <= 1; i += 0.01) {
+        setTimeout("SetOpa(" + (1 - i) + ")", i * duration);
+    }
+    setTimeout("FadeIn()", (duration + hidtime));
 }
+
 function FadeIn() {
-  for (i = 0; i <= 1; i += 0.01) {
-    setTimeout("SetOpa(" + i +")", i * duration);
-  }
-   setTimeout("fadeOut()", (duration + showtime));
+    for (i = 0; i <= 1; i += 0.01) {
+        setTimeout("SetOpa(" + i + ")", i * duration);
+    }
+    setTimeout("fadeOut()", (duration + showtime));
 }
